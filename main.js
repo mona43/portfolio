8e838b485140baa5ba46b3b768551365d208fba7
mona43 <manonaa43@gmail.com>
2024-09-23 02:28:43 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+// add class active////
+let header = document.querySelector("header")
+window.onscroll = function(){
+    if(this.scrollY>=50){
+        header.classList.add("active")
+    }
+    else{
+        header.classList.remove("active")
+    }
+};
